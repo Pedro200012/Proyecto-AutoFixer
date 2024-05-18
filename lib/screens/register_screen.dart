6f9eeec80c204e9bb706a9ffae095e39bbb7adbc
Sobrino Julian <sobrinojulian@protonmail.com>
2024-05-18 09:15:40 -0300

    register: vincula auth con firestore.collection('users')

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:go_router/go_router.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -12,7 +13,10 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   String _errorMessage = '';
 
   Future<void> _register() async {
@@ -22,10 +26,17 @@ class _RegisterScreenState extends State<RegisterScreen> {
         password: _passwordController.text,
       );
       final user = userCredential.user;
-      if (user != null && user.email == 'admin@example.com') {
-        context.go('/administrador');
-      } else {
-        context.go('/cliente');
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'name': _nameController.text,
+          'phone': _phoneController.text,
+          'email': _emailController.text,
+        });
+        if (user.email == 'admin@example.com') {
+          context.go('/administrador');
+        } else {
+          context.go('/cliente');
+        }
       }
     } on FirebaseAuthException catch (e) {
       setState(() {
@@ -45,6 +56,22 @@ class _RegisterScreenState extends State<RegisterScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(
+                labelText: 'Phone',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
             TextField(
               controller: _emailController,
               decoration: InputDecoration(
