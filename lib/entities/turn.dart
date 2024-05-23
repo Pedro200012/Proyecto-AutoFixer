52b4fb29e4d459debb4bed9096a818caa324bfe4
martin011001 <martingagliardi2001@gmail.com>
2024-05-23 17:59:57 -0300

    Screen de turnos + firebase y entidad turno

--- lib/entities/turn.dart
+++ lib/entities/turn.dart
@@ -1,28 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'user.dart';
 
 class Turn {
   final String id;
-  final String date;
-  final String hour;
+  final DateTime date;
+  final DocumentReference userRef;
   final String type;
   final String state;
+  late User user;
 
-  Turn({required this.id, required this.date, required this.hour, required this.type, required this.state});
+  Turn({required this.id, required this.date, required this.userRef, required this.type, required this.state});
 
   factory Turn.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Turn(
       id: doc.id,
-      date: data['date'] ?? '',
-      hour: data['hour'] ?? '',
+      date: (data['date'] as Timestamp).toDate(),
+      userRef: data['user'] as DocumentReference,
       type: data['type'] ?? '',
       state: data['state'] ?? '',
     );
   }
-    Map<String, dynamic> toFirestore() {
+
+  // Método para cargar los datos del usuario desde la referencia
+  Future<void> loadUser() async {
+    final userData = await userRef.get();
+    user = User.fromFirestore(userData); // Aquí usamos directamente el DocumentSnapshot
+  }
+
+  Map<String, dynamic> toFirestore() {
     return {
       'date': date,
-      'hour': hour,
+      'user': userRef,
       'type': type,
       'state': state,
     };
--- lib/screens/admin/_turnos_screen.dart
+++ lib/screens/admin/_turnos_screen.dart
@@ -1,4 +1,8 @@
+import 'package:aplicacion_taller/entities/turn.dart';
+import 'package:aplicacion_taller/entities/user.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class TurnosScreen extends StatelessWidget {
   const TurnosScreen({super.key});
@@ -7,12 +11,87 @@ class TurnosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Turnos'),
-        automaticallyImplyLeading: true, // This shows the back arrow
+        title: const Text('Turnos'),
+        automaticallyImplyLeading: true, // Esto muestra la flecha de retroceso
       ),
-      body: Center(
-        child: Text('Turnos Screen'),
+      // Usar StreamBuilder para escuchar los cambios en Firestore
+      body: StreamBuilder<QuerySnapshot>(
+        // Obtener la colección 'turns' desde Firestore
+        stream: FirebaseFirestore.instance.collection('turns').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Algo salió mal'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          // Obtener los datos del snapshot
+          final data = snapshot.requireData;
+
+          // Convertir los datos del snapshot a una lista de objetos Turn
+          List<Turn> turns =
+              data.docs.map((doc) => Turn.fromFirestore(doc)).toList();
+
+          // Pasar la lista de Turn a _ListTurnView
+          return _ListTurnView(turns: turns);
+        },
       ),
+    );
+  }
+}
+
+class _ListTurnView extends StatelessWidget {
+  final List<Turn> turns;
+
+  // Constructor para recibir la lista de Turn
+  const _ListTurnView({required this.turns});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: turns.length, // Número de elementos en la lista
+      itemBuilder: (context, index) {
+        final turn = turns[index];
+        return _TurnItem(turn: turn);
+      },
+    );
+  }
+}
+
+class _TurnItem extends StatelessWidget {
+  final Turn turn;
+
+  const _TurnItem({required this.turn});
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedDate = DateFormat('dd MMM yyyy, hh:mm a').format(turn.date);
+
+    return FutureBuilder<DocumentSnapshot>(
+        future: turn.userRef.get(), // Cargar el documento del usuario
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LinearProgressIndicator(); // Mostrar un indicador de carga
+          }
+          if (snapshot.hasError) {
+            return const Text('Error al cargar usuario');
+          }
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return const Text('Usuario no encontrado');
+          }
+
+          // Crear el objeto User desde el snapshot
+          User user = User.fromFirestore(snapshot.data!);
+
+          return Card(
+              child: ListTile(
+            title: Text(user.name), 
+            subtitle: Text(formattedDate), 
+          )
+        );
+      }
     );
   }
 }
