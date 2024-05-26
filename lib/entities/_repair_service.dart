cc48336c10e09760617e3f0dfb1c38411032fb3b
Your Namecls <you@example.com>
2024-05-26 17:06:03 -0300

    modif service para poder manejar reserva

--- lib/entities/_repair_service.dart
+++ lib/entities/_repair_service.dart
@@ -1,21 +1,37 @@
-// ignore_for_file: constant_identifier_names
-
 class Service {
+  final String id; // Nuevo campo id
   final String nombre;
   final double precio;
   final String descripcion;
   final Tamanio tamanio;
- 
 
-  Service({required this.nombre, required this.precio, required this.descripcion, required this.tamanio});
+  Service({
+    required this.id, // Agregar id al constructor
+    required this.nombre,
+    required this.precio,
+    required this.descripcion,
+    required this.tamanio,
+  });
 }
 
 enum Tamanio {
-  CHICO,
-  GRANDE,
+  chico,
+  grande,
 }
 
 List<Service> services = [
-  Service(nombre: 'pintra', precio: 30000, descripcion: 'nueva capa de pintura para todo el vehiculo', tamanio: Tamanio.GRANDE),
-  Service(nombre: 'pulido', precio: 15000, descripcion: 'pulido para todo el vehiculo', tamanio: Tamanio.CHICO),
+  Service(
+    id: '1', // ID del servicio 1
+    nombre: 'pintra',
+    precio: 30000,
+    descripcion: 'nueva capa de pintura para todo el vehiculo',
+    tamanio: Tamanio.grande,
+  ),
+  Service(
+    id: '2', // ID del servicio 2
+    nombre: 'pulido',
+    precio: 15000,
+    descripcion: 'pulido para todo el vehiculo',
+    tamanio: Tamanio.chico,
+  ),
 ];
