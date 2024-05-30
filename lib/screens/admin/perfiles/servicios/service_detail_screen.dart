d127f7b42ff0bf4958e34bf9df68d36336acba46
Valentino <valentinotartari@gmail.com>
2024-05-30 20:48:58 -0300

    service detail

--- lib/screens/admin/perfiles/servicios/service_detail_screen.dart
+++ lib/screens/admin/perfiles/servicios/service_detail_screen.dart
@@ -1,38 +1,85 @@
-import 'dart:developer';
-
+import 'package:aplicacion_taller/entities/service.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ServicieDetailScreen extends StatelessWidget {
-  final Service service;
+class ServiceDetailScreen extends StatefulWidget {
+  final String serviceId;
 
-  const ServicieDetailScreen({super.key, required this.service});
+  ServiceDetailScreen({required this.serviceId});
+
+  @override
+  _ServiceDetailScreenState createState() => _ServiceDetailScreenState();
+}
+
+class _ServiceDetailScreenState extends State<ServiceDetailScreen> {
+  Future<Service> _fetchService() async {
+    DocumentSnapshot doc = await FirebaseFirestore.instance
+        .collection('services')
+        .doc(widget.serviceId)
+        .get();
+    return Service.fromFirestore(doc);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalle'),
+        title: const Text('Service Details'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // child: Column(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     Text(
-        //       service.name,
-        //       style: TextStyle(
-        //         fontSize: 24.0,
-        //         fontWeight: FontWeight.bold,
-        //       ),
-        //     ),
-        //     SizedBox(height: 20.0),
-        //     Text(
-        //       service.description,
-        //       style: TextStyle(fontSize: 18.0),
-        //     ),
-        //     // Aquí puedes agregar más detalles o funcionalidades de edición
-        //   ],
-        // ),
+      body: FutureBuilder<Service>(
+        future: _fetchService(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error fetching service details'));
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text('Service not found'));
+          }
+
+          Service service = snapshot.data!;
+
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Service Name: ${service.name}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Price: \$${service.price.toStringAsFixed(2)}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Acción para editar
+                        },
+                        child: const Text('Edit'),
+                      ),
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Acción para eliminar
+                        },
+                        // ignore: sort_child_properties_last
+                        child: const Text('Delete'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
